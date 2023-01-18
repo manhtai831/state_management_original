d68fb294e43685acd2bbb8c1f91440af714da6a5
DO Manh Tai <manhtai831@gmail.com>
2023-01-18 10:04:54 +0700

    update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+
+* Hotfix update state.
+
 ## 0.0.1
 
 * TODO: Describe initial release.
--- lib/widget_state_notifier/state_builder.dart
+++ lib/widget_state_notifier/state_builder.dart
@@ -4,7 +4,7 @@ import 'package:state_management_original/state_management_original.dart';
 typedef OnStaterRebuild = Widget Function(Stater stater);
 
 class StateBuilder extends StatefulWidget {
-  const StateBuilder(this.stater, {Key? key, required this.builder}) : super(key: key);
+  StateBuilder(this.stater, {Key? key, required this.builder}) : super(key: stater.key);
   final OnStaterRebuild builder;
   final Stater stater;
   @override
